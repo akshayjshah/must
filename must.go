f758561cd61d9da57b9e1cbe3c18d633820e73ec
Akshay Shah <akshay@akshayshah.org>
2023-05-08 21:43:09 -0700

    Fix GoDoc link to template

--- must.go
+++ must.go
@@ -1,11 +1,12 @@
 // Package must provides a generic equivalent of the standard library's
-// [template.Must] or [regexp.MustCompile].
+// [text/template.Must] or [regexp.MustCompile].
 package must
 
 // Must adapts a typical Go constructor to either panic or succeed. This is
 // usually undesirable, but it's occasionally useful when creating global
 // variables using a pure function and static inputs. Think of it as a generic
-// version of the standard library's [template.Must] or [regexp.MustCompile].
+// version of the standard library's [text/template.Must] or
+// [regexp.MustCompile].
 func Must[T any](val T, err error) T {
 	if err != nil {
 		panic(err)
